9d2a0986fb8c7e593f2fb26ec65726999380a018
Eligio93 <eligio.cristantielli@gmail.com>
2023-03-17 14:19:23 +1100

    finish round funtion to compare results of 1 game

--- javascript.js
+++ javascript.js
@@ -9,5 +9,34 @@ function getComputerChoice(pcChoice){
     return pcChoice;
 }
 
+function round(playerSelection,pcSelection){
+    if(playerSelection===pcSelection){
+        return "Draw! Your weapons are the same";
+    }else if(playerSelection==="rock"&& pcSelection==="paper"){
+         pcScore=pcScore+1;
+         return "You Lose!Paper beats Rock";
+    }else if(playerSelection==="paper"&& pcSelection==="scissor"){
+         pcScore=pcScore+1;
+         return "You Lose!Scissor beats paper";
+    }else if(playerSelection==="scissor"&& pcSelection==="rock"){
+         pcScore=pcScore+1;
+         return "You Lose!Rock beats Scissor";
+    }else if(playerSelection==="paper"&& pcSelection==="rock"){
+         playerScore=playerScore+1;
+         return "You Win!Paper beats Rock";
+    }else if(playerSelection==="scissor"&& pcSelection==="paper"){
+         playerScore=playerScore+1;
+         return "You Win!Scissor beats paper";
+    }else if(playerSelection==="rock"&& pcSelection==="scissor"){
+        playerScore=playerScore+1;
+        return "You Win!Paper beats Rock";
+    }else{
+        return "there is a problem"  }
+
+
+}
+
 console.log(pcSelection);
-console.log(playerSelection.toLowerCase());
+console.log(playerSelection);
+console.log(round(playerSelection,pcSelection));
+console.log(playerScore,pcScore);
