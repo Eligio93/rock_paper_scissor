610f527f26916ab377a1c00054fd6ba2dfd3bffc
Eligio93 <eligio.cristantielli@gmail.com>
2023-04-08 10:09:12 +1000

    create button event Listener that plays round

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-let pcChoice= ["rock","paper","scissor"];
+let pcChoice= ["rock","paper","scissors"];
 let pcSelection= "";
+
 let playerSelection="";
 let playerScore=0;
 let pcScore=0;
@@ -17,19 +18,19 @@ function round(playerSelection,pcSelection){
     }else if(playerSelection==="rock"&& pcSelection==="paper"){
          pcScore=pcScore+1;
          return "You Lose!Paper beats Rock";
-    }else if(playerSelection==="paper"&& pcSelection==="scissor"){
+    }else if(playerSelection==="paper"&& pcSelection==="scissors"){
          pcScore=pcScore+1;
          return "You Lose!Scissor beats paper";
-    }else if(playerSelection==="scissor"&& pcSelection==="rock"){
+    }else if(playerSelection==="scissors"&& pcSelection==="rock"){
          pcScore=pcScore+1;
          return "You Lose!Rock beats Scissor";
     }else if(playerSelection==="paper"&& pcSelection==="rock"){
          playerScore=playerScore+1;
          return "You Win!Paper beats Rock";
-    }else if(playerSelection==="scissor"&& pcSelection==="paper"){
+    }else if(playerSelection==="scissors"&& pcSelection==="paper"){
          playerScore=playerScore+1;
          return "You Win!Scissor beats paper";
-    }else if(playerSelection==="rock"&& pcSelection==="scissor"){
+    }else if(playerSelection==="rock"&& pcSelection==="scissors"){
         playerScore=playerScore+1;
         return "You Win!Paper beats Rock";
     }else{
@@ -37,10 +38,12 @@ function round(playerSelection,pcSelection){
 
 
 }
+
+
 //the function below repeat the round for 5 times and declare the winner, the loser or if the game ends in draw
-function game(){
+/*function game(){
 for(let cont=0;cont<5;cont++){
-    playerSelection=prompt("Type rock, paper or scissor").toLowerCase(); 
+    playerSelection=prompt("Type rock, paper or scissors").toLowerCase(); 
     console.log(playerSelection);
     pcSelection=getComputerChoice(pcChoice);
     console.log(pcSelection);
@@ -58,14 +61,20 @@ if(playerScore>pcScore){
     finalMessage="The game is Draw";
     return finalMessage;
 }
-}
-
-console.log(game());
+}*/
 
 
-
-
-
+//Everytime i click one button a round is been played
+let btn=document.querySelectorAll("button").forEach(function(e){
+   e.addEventListener("click",function(){
+    playerSelection=e.id;
+    pcSelection=getComputerChoice(pcChoice);
+    console.log(playerSelection);
+    console.log(pcSelection);
+    console.log(round(playerSelection,pcSelection));
+    
+   })
+});
 
 
 
